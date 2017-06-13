197b3c297ab81a28f3be4a1f0cf00e155bb3297d
ketan <code.ketan@gmail.com>
2017-06-13 15:58:00 +0530

    commit #3

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Task = require('./api/models/todoListModel'),
+  bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/tododb'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/todoListRoutes');
+routes(app);
 
 app.listen(port);
 
