347377f35a0d4ec6677442c7cc96cf47496181c2
ketan <code.ketan@gmail.com>
2017-06-13 16:21:09 +0530

    commit #4

--- server.js
+++ server.js
@@ -20,4 +20,6 @@ app.listen(port);
 
 console.log('todo list RESTful API server started on: ' + port);
 
-
+app.use(function(req, res) {
+  res.status(404).send({url: req.originalUrl + ' not found'})
+});
